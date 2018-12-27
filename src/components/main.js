6eeec1f5c91156a21a702dc1c3109a8561d704bf
Rachit Malik <rachit@Rachits-MacBook-Pro.local>
2018-12-27 13:16:21 +0530

    Added components

---
+++ src/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './landingpage';
+
+
+
+const Main =() => {
+  <Switch>
+    <Route exact path = "/" component = {LandingPage} />
+  </Switch>
+
+
+}
+export default Main; 
